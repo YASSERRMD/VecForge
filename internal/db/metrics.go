064f2bd9bd4fa4c1e0fb71abc5df2b89e80671b8
YASSERRMD <arafath.yasser@gmail.com>
2026-02-19 13:31:26 +0400

    fix(phase4): fix metrics implementation

--- internal/db/metrics.go
+++ internal/db/metrics.go
@@ -2,26 +2,23 @@ package db
 
 import (
 	"sync/atomic"
-	"time"
 )
 
 type Metrics struct {
-	searches   uint64
-	errors     uint64
-	latencyUs  uint64
+	searches  uint64
+	errors    uint64
+	latencyUs uint64
 }
 
 func NewMetrics() *Metrics { return &Metrics{} }
 
 func (m *Metrics) IncSearches()   { atomic.AddUint64(&m.searches, 1) }
 func (m *Metrics) IncErrors()     { atomic.AddUint64(&m.errors, 1) }
-func (m *Metrics) AddLatency(us int64) {
-	atomic.AddUint64(&m.latencyUs, uint64(us))
-}
+func (m *Metrics) AddLatency(us int64) { atomic.AddUint64(&m.latencyUs, uint64(us)) }
 
-func (m *Metrics) Searches() uint64   { return atomic.LoadUint64(&m.searches) }
-func (m *Metrics) Errors() uint64     { return atomic.LoadUint64(&m.errors) }
-func (m *Metrics) LatencyUs() uint64.LoadUint64(&m.latencyUs  { return atomic) }
+func (m *Metrics) Searches() uint64  { return atomic.LoadUint64(&m.searches) }
+func (m *Metrics) Errors() uint64    { return atomic.LoadUint64(&m.errors) }
+func (m *Metrics) LatencyUs() uint64 { return atomic.LoadUint64(&m.latencyUs) }
 
 func (m *Metrics) AvgLatencyUs() int64 {
 	s := m.Searches()
@@ -29,21 +26,4 @@ func (m *Metrics) AvgLatencyUs() int64 {
 		return 0
 	}
 	return int64(m.LatencyUs() / s)
-}
-
-type MetricsCollector struct {
-	providers map[string]*Metrics
-}
-
-func NewMetricsCollector() *MetricsCollector {
-	return &MetricsCollector{providers: make(map[string]*Metrics)}
-}
-
-func (mc *MetricsCollector) Get(name string) *Metrics {
-	if m, ok := mc.providers[name]; ok {
-		return m
-	}
-	m := NewMetrics()
-	mc.providers[name] = m
-	return m
 }
