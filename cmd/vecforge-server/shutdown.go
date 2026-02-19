c4534a54580336fd9e1285d64be14c579dfcd759
YASSERRMD <arafath.yasser@gmail.com>
2026-02-19 14:53:15 +0400

    feat(phase7): add graceful shutdown handler

---
+++ cmd/vecforge-server/shutdown.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func gracefulShutdown(srv *http.Server, timeout time.Duration) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown: %v", err)
+	}
+	log.Println("Server exited")
+}
